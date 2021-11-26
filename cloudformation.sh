d7f5f8098245c7f7d01a15177b499d67b564ddcf
n-tangible <94999880+n-tangible@users.noreply.github.com>
2021-11-26 23:46:00 +0000

    Create simple S3 hosted site
    Generation of a S3 hosted site with http endpoint, with supporting
    shell script to deploy cloudformation stack and upload site assets.

---
+++ cloudformation.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env sh
+
+# Input parameters required are in order:
+BUCKET_NAME=${1//_/-}
+
+if [ "$BUCKET_NAME" == "" ]; then
+	BUCKET_NAME="checkout-devops-challenge"
+fi
+
+declare -a PARAMETERS=(
+	"ParameterKey=BucketName,ParameterValue=$BUCKET_NAME"
+)
+PARAMETERS_JOINED=$(IFS=$' '; echo "${PARAMETERS[*]}")
+
+STACK_NAME="$BUCKET_NAME-stack"
+
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+TEMPLATE_BODY="file://$DIR/cloudformation.json"
+
+if aws cloudformation list-stacks --query "StackSummaries[*].StackName" | grep -q $STACK_NAME ; then
+	aws cloudformation update-stack --stack-name $STACK_NAME --template-body $TEMPLATE_BODY --parameters $PARAMETERS_JOINED
+else
+	aws cloudformation create-stack --stack-name $STACK_NAME --template-body $TEMPLATE_BODY --parameters $PARAMETERS_JOINED
+fi
+
+if aws s3 ls | grep -q $BUCKET_NAME ; then
+    aws s3 cp $DIR/checkout/ s3://$BUCKET_NAME/ --sse --recursive
+fi
